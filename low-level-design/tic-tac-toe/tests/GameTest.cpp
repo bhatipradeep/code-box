a61715c6317e99b1c10c75ba0b618107f987422d
Pradeepsingh Bhati <bh.pradeepsingh@gmail.com>
2021-04-03 01:10:28 +0530

    [Add] Game Test for playing 2-player game in terminal

---
+++ low-level-design/tic-tac-toe/tests/GameTest.cpp
@@ -0,0 +1,36 @@
+#include "../src/Game.h"
+#include "../src/Player.h"
+#include <iostream>
+#include <string>
+
+int main(){
+    int size;
+    std::cout<<"Enter game dimensions (Eg.3) :";
+    std::cin>>size;
+
+    Game game = Game(size);
+    
+    std::string name;
+    char symbol;
+    std::cout<<"Enter Player1 Name:";
+    std::cin>>name;
+    std::cout<<"Enter Player1 Symbol(length=1):";
+    std::cin>>symbol;
+    Player player1(name,symbol);
+    std::cout<<"Enter Player2 Name:";
+    std::cin>>name;
+    std::cout<<"Enter Player2 Symbol(length=1):";
+    std::cin>>symbol;
+    Player player2(name,symbol);
+
+    game.addPlayers(player1,player2);
+
+    std::cout<<"Game will start with player - 1 move ! "<<std::endl;
+    while(true){
+        std::cout<<"Enter x <space> y :";
+        int x,y;
+        std::cin>>x>>y;
+        game.makeMove(x,y);
+
+    }
+}
--- low-level-design/tic-tac-toe/tests/PlayerTest.cpp
+++ low-level-design/tic-tac-toe/tests/PlayerTest.cpp
@@ -2,7 +2,7 @@
 #include<iostream>
 
 int main(){
-    Player p1("abc","1");
+    Player p1("abc",'1');
     std::cout<<p1.getName()<<std::endl;
     std::cout<<p1.getSymbol()<<std::endl;
 }
