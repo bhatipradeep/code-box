206e32cc5e0ae51432cac24948ec1a851cc7c7de
Pradeepsingh Bhati <bh.pradeepsingh@gmail.com>
2021-04-08 22:53:18 +0530

    [Add] ShoppingCart(Client) Class flexible for using any payment and invoice strategies

---
+++ oops-implementation/design-patterns/strategy/src/ShoppingCart.h
@@ -0,0 +1,27 @@
+#include <vector>
+#include "src/strategies/invoice/IInvoice.h"
+#include "src/strategies/payment/IPayment.h"
+
+class Item
+{
+};
+
+class ShoppingCart
+{
+private:
+    std::vector<Item> items;
+    IInvoice *invoiceAlgo;
+
+public:
+    ShoppingCart(IInvoice *invoiceAlgo)
+    {
+        this->invoiceAlgo = invoiceAlgo;
+    }
+
+    void addItems(Item item)
+    {
+        items.push_back(item);
+    }
+
+    void payment(IPayment *paymentAlgo){}
+};
